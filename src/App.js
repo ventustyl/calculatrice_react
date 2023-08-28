e050b7ce61c2ebadee52a53b2f7a57a9ea2c7cfd
Eric Venturino <eric.venturino@orange.fr>
2023-08-28 17:18:34 +0200

    projet calc

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    if (value === "=") {
+      try {
+        const calculatedResult = eval(expression);
+        setResult(calculatedResult);
+      } catch (error) {
+        setResult("Error");
+      }
+      setExpression("");
+    } else if (value === "C") {
+      setExpression("");
+      setResult("");
+    } else {
+      setExpression(expression + value);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Calculatrice</h1>
+      <div className="calculator">
+        <div className="screen">
+          {expression}
+          <br />
+          {result}
+        </div>
+        <div className="buttons">
+          <button className="button" onClick={() => handleButtonClick("7")}>
+            7
+          </button>
+          <button className="button" onClick={() => handleButtonClick("8")}>
+            8
+          </button>
+          <button className="button" onClick={() => handleButtonClick("9")}>
+            9
+          </button>
+          <button className="button" onClick={() => handleButtonClick("/")}>
+            /
+          </button>
+          <button className="button" onClick={() => handleButtonClick("4")}>
+            4
+          </button>
+          <button className="button" onClick={() => handleButtonClick("5")}>
+            5
+          </button>
+          <button className="button" onClick={() => handleButtonClick("6")}>
+            6
+          </button>
+          <button className="button" onClick={() => handleButtonClick("*")}>
+            *
+          </button>
+          <button className="button" onClick={() => handleButtonClick("1")}>
+            1
+          </button>
+          <button className="button" onClick={() => handleButtonClick("2")}>
+            2
+          </button>
+          <button className="button" onClick={() => handleButtonClick("3")}>
+            3
+          </button>
+          <button className="button" onClick={() => handleButtonClick("-")}>
+            -
+          </button>
+          <button className="button" onClick={() => handleButtonClick("0")}>
+            0
+          </button>
+          <button className="button" onClick={() => handleButtonClick("C")}>
+            C
+          </button>
+          <button className="button" onClick={() => handleButtonClick("+")}>
+            +
+          </button>
+          <button className="button" onClick={() => handleButtonClick("=")}>
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
